1f5f80aca506c757a1bf19d0725f2fe63d26a4ec
klukas17 <mihael.milicevic3@gmail.com>
2023-04-29 15:22:18 +0200

    genotype: added JobSpecificationsParser

---
+++ code/jobs/headers/JobType.h
@@ -0,0 +1,17 @@
+//
+// Created by mihael on 29/04/23.
+//
+
+#ifndef SCHEDULING_JOBTYPE_H
+#define SCHEDULING_JOBTYPE_H
+
+
+class JobType {
+protected:
+    long id;
+public:
+    JobType(long id);
+};
+
+
+#endif //SCHEDULING_JOBTYPE_H
--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include "MachineSpecificationsParser.h"
 #include "MachineTopologyParser.h"
+#include "JobSpecificationsParser.h"
 #include "MachineType.h"
 #include "Topology.h"
+
+std::string dir = "../examples/example_01/";
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
     MachineSpecificationsParser* machineSpecificationsParser = new MachineSpecificationsParser();
-    std::map<long, MachineType*> machine_type_map = machineSpecificationsParser->parse("../examples/example_01/machine_specifications.yaml");
+    std::map<long, MachineType*> machine_type_map = machineSpecificationsParser->parse(dir + "machine_specifications.yaml");
 
     MachineTopologyParser* machineTopologyParser = new MachineTopologyParser();
-    Topology* topology = machineTopologyParser->parse("../examples/example_01/machine_topology.yaml", machine_type_map);
+    Topology* topology = machineTopologyParser->parse(dir + "machine_topology.yaml", machine_type_map);
+
+    JobSpecificationsParser* jobSpecificationsParser = new JobSpecificationsParser();
+    std::map<long, JobType*> job_type_map = jobSpecificationsParser->parse(dir + "job_specifications.yaml");
 
     return 0;
 }
