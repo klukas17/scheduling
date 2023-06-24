b2ea608c796a3243311e1496c60ac2ef901a60aa
klukas17 <mihael.milicevic3@gmail.com>
2023-06-24 22:40:12 +0200

    events: added forbidden_machine_types field

--- code/jobs/headers/JobType.h
+++ code/jobs/headers/JobType.h
@@ -13,6 +13,7 @@ protected:
     long id;
     std::map<long, long> processing_times;
     std::set<long> job_families;
+    std::set<long> forbidden_machine_types;
 public:
     explicit JobType(long id);
     [[nodiscard]] long getId() const;
@@ -20,6 +21,8 @@ public:
     void setProcessingTime(long machine_id, long processing_time);
     std::set<long> getJobFamilies();
     void addJobFamily(long job_family_id);
+    std::set<long> getForbiddenMachineTypes();
+    void addForbiddenMachineType(long machine_type_id);
 };
 
 
