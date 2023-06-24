524addb25bd0e668d3a451486d18b584a60469b4
klukas17 <mihael.milicevic3@gmail.com>
2023-06-24 20:12:01 +0200

    events: added release_date, due_date and weight fields

--- code/jobs/sources/Job.cpp
+++ code/jobs/sources/Job.cpp
@@ -4,9 +4,12 @@
 
 #include "Job.h"
 
-Job::Job(long id, JobType *job_type) {
+Job::Job(long id, JobType* job_type, long release_date, long due_date, long weight) {
     this->id = id;
     this->job_type = job_type;
+    this->release_date = release_date;
+    this->due_date = due_date;
+    this->weight = weight;
 }
 
 long Job::getId() const {
@@ -15,6 +18,18 @@ long Job::getId() const {
 
 JobType *Job::getJobType() {
     return job_type;
+}
+
+long Job::getReleaseDate() {
+    return release_date;
+}
+
+long Job::getDueDate() {
+    return due_date;
+}
+
+long Job::getWeight() {
+    return weight;
 }
 
 std::vector<long> Job::getProcessingRoute() {
