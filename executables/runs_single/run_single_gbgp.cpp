b47700b614a697fd45ef98eff3992794df02cba6
klukas17 <mihael.milicevic3@gmail.com>
2024-06-08 16:11:00 +0200

    online_scheduling_algorithms: added single run programs for all algorithms

---
+++ executables/runs_single/run_single_gbgp.cpp
@@ -0,0 +1,152 @@
+//
+// Created by mihael on 6/5/24.
+//
+
+#include <iostream>
+
+#include "DiscountedTotalWeightedCompletionTimeObjectiveFunction.h"
+#include "GraphBasedGeneticProgrammingCombinationOperator.h"
+#include "GraphBasedGeneticProgrammingCreationOperator.h"
+#include "GraphBasedGeneticProgrammingGenotypeBlueprint.h"
+#include "GraphBasedGeneticProgrammingPerturbationOperator.h"
+#include "GraphBasedGeneticProgrammingSerializationOperator.h"
+#include "Job.h"
+#include "JobSequenceParser.h"
+#include "JobSpecificationsParser.h"
+#include "MachineSpecificationsParser.h"
+#include "MachineTopologyParser.h"
+#include "MachineTypeMap.h"
+#include "OnlineSchedulingAlgorithmClusterCombinationOperatorWithFineGranularity.h"
+#include "OnlineSchedulingAlgorithmClusterCreationOperator.h"
+#include "OnlineSchedulingAlgorithmClusterGenotypeBlueprint.h"
+#include "OnlineSchedulingAlgorithmClusterPerturbationOperator.h"
+#include "OnlineSchedulingAlgorithmClusterSerializationOperator.h"
+#include "RandomPathTopologyEnumerator.h"
+#include "SchedulingEvaluationFunction.h"
+#include "SchedulingMetaAlgorithm.h"
+#include "SteadyStateGeneticAlgorithm.h"
+#include "Topology.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <string_argument>" << std::endl;
+        return 1;
+    }
+
+    std::string dir = argv[1];
+
+    MachineTypeMap* machine_type_map = MachineSpecificationsParser::parse(dir + "machine_specifications.yaml");
+    JobTypeMap* job_type_map = JobSpecificationsParser::parse(dir + "job_specifications.yaml");
+    machine_type_map->constructSetupAndBatchRules(job_type_map);
+    Topology* topology = MachineTopologyParser::parse(dir + "machine_topology.yaml", machine_type_map);
+    std::map<long, Job*> jobs_train_set = JobSequenceParser::parse(dir + "job_sequence_train.yaml", machine_type_map, job_type_map, topology);
+    std::map<long, Job*> jobs_test_set = JobSequenceParser::parse(dir + "job_sequence_test.yaml", machine_type_map, job_type_map, topology);
+
+    int population_size = 150;
+    // *******************************
+    int max_number_of_nodes = 300;
+    double perturbation_rate = 0.2;
+    int max_nodes_to_delete = 1;
+    int max_nodes_to_insert = 4;
+    int max_number_of_nodes_to_crossover = 10;
+    double proceed_in_branch_chance = 0.6;
+
+    auto objective_function = new DiscountedTotalWeightedCompletionTimeObjectiveFunction();
+    auto sub_evaluation_function = new SchedulingEvaluationFunction(
+        objective_function,
+        topology,
+        jobs_train_set,
+        false,
+        ""
+    );
+    auto evaluation_function = new SchedulingEvaluationFunction(
+        objective_function,
+        topology,
+        jobs_test_set,
+        false,
+        ""
+    );
+
+    auto sub_blueprint = new GraphBasedGeneticProgrammingGenotypeBlueprint(
+        max_number_of_nodes,
+        -1,
+        1
+    );
+    auto sub_creation_operator = new GraphBasedGeneticProgrammingCreationOperator(sub_blueprint);
+    auto sub_combination_operator = new GraphBasedGeneticProgrammingCombinationOperator(
+        sub_blueprint,
+        max_number_of_nodes_to_crossover,
+        proceed_in_branch_chance
+    );
+    auto sub_perturbation_operator = new GraphBasedGeneticProgrammingPerturbationOperator(
+        sub_blueprint,
+        perturbation_rate,
+        max_nodes_to_delete,
+        max_nodes_to_insert
+    );
+    auto sub_serialization_operator = new GraphBasedGeneticProgrammingSerializationOperator(sub_blueprint);
+
+    auto genotype_blueprint = new OnlineSchedulingAlgorithmClusterGenotypeBlueprint(topology);
+    auto creation_operator = new OnlineSchedulingAlgorithmClusterCreationOperator(
+        genotype_blueprint,
+        sub_creation_operator
+    );
+    auto perturbation_operator = new OnlineSchedulingAlgorithmClusterPerturbationOperator(
+        sub_perturbation_operator
+    );
+    auto combination_operator = new OnlineSchedulingAlgorithmClusterCombinationOperatorWithFineGranularity(
+        sub_combination_operator
+    );
+    auto serialization_operator = new OnlineSchedulingAlgorithmClusterSerializationOperator(
+        topology,
+        sub_serialization_operator
+    );
+
+    int machines_with_algorithms = 0;
+    for (auto [element_id, element] : topology->getTopologyElementsMap()) {
+        auto element_type = element->getTopologyElementType();
+        if (element_type == MACHINE_TOPOLOGY_ELEMENT || element_type == PARALLEL_GROUP_TOPOLOGY_ELEMENT || element_type == OPEN_GROUP_TOPOLOGY_ELEMENT) {
+            machines_with_algorithms++;
+        }
+    }
+
+    auto topology_enumerator = new RandomPathTopologyEnumerator(topology, 0.8);
+
+    auto sub_algorithm_population = new Population(population_size);
+    auto algorithm_population = new Population(1);
+
+    auto sub_algorithm = new SteadyStateGeneticAlgorithm(
+        sub_evaluation_function,
+        creation_operator,
+        perturbation_operator,
+        combination_operator,
+        population_size,
+        500
+    );
+
+    auto algorithm = new SchedulingMetaAlgorithm(
+        evaluation_function,
+        creation_operator,
+        perturbation_operator,
+        combination_operator,
+        topology_enumerator,
+        sub_algorithm,
+        sub_algorithm_population,
+        100
+    );
+
+    algorithm->optimize(algorithm_population);
+
+    auto unit = algorithm_population->getGenotype(0);
+    std::cout << "Genotype:" << std::endl;
+    for (auto line : serialization_operator->serialize(unit->genotype)) {
+        std::cout << line << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "RESULT: " << unit->fitness_score << std::endl;
+
+    delete sub_algorithm_population;
+    delete algorithm_population;
+    delete sub_algorithm;
+    delete algorithm;
+}
